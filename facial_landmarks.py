1cef95b46cce2b7516e5e2f7895bb163248be25e
Ben Lerner <benlerner95@gmail.com>
2017-07-16 23:31:23 -0400

    managed to superimpose neutral face emoji over my face

--- facial_landmarks.py
+++ facial_landmarks.py
@@ -28,6 +28,9 @@ image = cv2.imread(args["image"])
 image = imutils.resize(image, width=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# TODO: replace with a real path
+emojiImg = cv2.imread("/Users/benlerner/Desktop/computer_vision/facial-landmarks/images/emoji/Neutral_Face_Emoji.png", -1)
+
 # detect faces in the grayscale image
 rects = detector(gray, 1)
 
@@ -41,27 +44,84 @@ for (i, rect) in enumerate(rects):
 
     # convert dlib's rectangle to a OpenCV-style bounding box
     # [i.e., (x, y, w, h)], then draw the face bounding box
-    (x, y, w, h) = face_utils.rect_to_bb(rect)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    (x, y, face_w, face_h) = face_utils.rect_to_bb(rect)
+    cv2.rectangle(image, (x, y), (x + face_w, y + face_h), (0, 255, 0), 2)
+
+    # configure emoji img
+    emojiImg = imutils.resize(emojiImg, width=face_w, height=face_h)
+    emojiMask = emojiImg[:, :, 3]
+    emojiImg = emojiImg[:, :, 0:3]
+    emojiMaskInv = cv2.bitwise_not(emojiMask)
+
+
+    # the region of interest is the face
+    roi_gray = gray[y:y+face_h, x:x+face_w]
+    # TODO: hack
+    roi_color = image[y:y+face_h+1, x:x+face_w]
+
+    # TODO: this will be different if I choose not to use the face bounding box
+    print(roi_color.shape)
+    print(emojiMaskInv.shape)
+    roi_bg = cv2.bitwise_and(roi_color, roi_color, mask = emojiMaskInv)
+#
+    roi_fg = cv2.bitwise_and(emojiImg, emojiImg, mask = emojiMask)
+    dst = cv2.add(roi_bg, roi_fg)
+    roi_color[:,:] = dst
+
+    # cv2.imshow('dst', image)
+#
+#    dst = cv2.add(roi_bg, roi_fg)
 
     # show the face number
     # cv2.putText(image, "Face #{}".format(i + 1), (x - 10, y - 10),
         # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-    leftLipIndices = itemgetter(48, 49, 59, 60)
-    rightLipIndices = itemgetter(53, 54, 55, 64)
-    leftLip = np.mean(leftLipIndices(shape), axis=0)
-    rightLip = np.mean(rightLipIndices(shape), axis=0)
+    # eyes
     leftEye = np.mean(shape[36:42, :], axis=0)
     rightEye = np.mean(shape[43:48, :], axis=0)
-    keyPoints = [leftLip, rightLip, leftEye, rightEye]
+
+    # lip edges
+    # leftLipIndices = itemgetter(48, 49, 59, 60)
+    # rightLipIndices = itemgetter(53, 54, 55, 64)
+    # leftLip = np.mean(leftLipIndices(shape), axis=0)
+    # rightLip = np.mean(rightLipIndices(shape), axis=0)
+    # keyFacePoints = np.float32([leftEye, rightEye, leftLip, rightLip])
+
+    # middle of mouth
+    mouth = np.mean(shape[60:68, :], axis=0)
+    keyFacePoints = np.float32([leftEye, rightEye, mouth])
+
+    leftEmojiEye = (223, 262)
+    rightEmojiEye = (412, 262)
+    # leftEmojiLip = (215, 414)
+    # rightEmojiLip = (420, 414)
+    emojiMouth = (320, 414)
+
+    # keyEmojiPoints = np.float32([leftEmojiEye, rightEmojiEye, leftEmojiLip, rightEmojiLip])
+    keyEmojiPoints = np.float32([leftEmojiEye, rightEmojiEye, emojiMouth])
+
+    # perspective transformation
+    # M = cv2.getPerspectiveTransform(keyEmojiPoints, keyFacePoints)
+    # dst = cv2.warpPerspective(emojiImg, M, (640, 640))
+
+    # affine transformation
+    # M = cv2.getAffineTransform(keyEmojiPoints, keyFacePoints)
+    # dst = cv2.warpAffine(emojiImg, M, (640, 640))
+    # cv2.imshow("warped", dst)
+
     # loop over the (x, y)-coordinates for the facial landmarks
     # and draw them on the image
-    for float_x, float_y in keyPoints:
-        x = int(float_x)
-        y = int(float_y)
-        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+    # for float_x, float_y in keyFacePoints:
+        # x = int(float_x)
+        # y = int(float_y)
+        # cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+    # for float_x, float_y in keyEmojiPoints:
+        # x = int(float_x)
+        # y = int(float_y)
+        # cv2.circle(emojiImg, (x, y), 1, (0, 0, 255), -1)
 
 # show the output image with the face detections + facial landmarks
+# cv2.imshow('emoj', emojiImg)
 cv2.imshow("Output", image)
 cv2.waitKey(0)
