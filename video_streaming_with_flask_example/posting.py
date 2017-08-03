53ec416b4068716413df22ef281925c84e7ca51d
Ben Lerner <benlerner95@gmail.com>
2017-08-03 18:54:21 -0400

    quick little changes before I clean it up

--- video_streaming_with_flask_example/posting.py
+++ video_streaming_with_flask_example/posting.py
@@ -62,4 +62,4 @@ def index():
 #                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='localhost', debug=True)
